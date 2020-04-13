eceda5c7d4161ad15a36d27baa4fad383a729ca0
david <daviddecastro42@live.co.uk>
2020-04-13 21:10:19 +0100

    create crounty api and connected with chart component and create bar the componet to see the gobal of eath country

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import { Cards, Chart, CountryPicker } from './components';
 
 class App extends React.Component {
   state = {
-    data: {}
+    data: {},
+    country: ''
   };
 
   async componentDidMount() {
@@ -19,14 +20,20 @@ class App extends React.Component {
     this.setState({ data: fetchedData });
   }
 
+  handleCountryChange = async country => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container}>
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
